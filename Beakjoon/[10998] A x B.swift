09511110a2c87918bc5972e740031d1071cb5c09
odong-Tree <ss12357@naver.com>
2021-05-21 20:22:28 +0900

    add 백준 10998

---
+++ Beakjoon/[10998] A x B.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  beakjoon
+//
+//  Created by 김동빈 on 2021/05/21.
+//
+
+var input = readLine()!.split(separator: " ")
+var result = Int(input[0])! * Int(input[1])!
+print(result)
